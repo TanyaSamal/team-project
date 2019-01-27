a8f09262c33be86b46ebe3fffdd31b2afc1298ec
Tanya Samal <solesytto@gmail.com>
2019-01-27 15:52:29 +0300

    #9/feat: translate daily author from json

--- js/translate.js
+++ js/translate.js
@@ -13,24 +13,26 @@ window.onload = () => {
         document.querySelector('#navbardrop').innerHTML = resultJson.values.currantLang;
         document.querySelector('.dropdown-item:first-child').innerHTML = resultJson.values.lang1;
         document.querySelector('.dropdown-item:last-child').innerHTML = resultJson.values.lang2;
-        document.querySelector('h1').innerHTML = resultJson.values.h1;
-        document.querySelector('.lead').innerHTML = resultJson.values.lead;
-        document.querySelector('.about h2').innerHTML = resultJson.values.about;
-        document.querySelector('.about p').innerHTML = resultJson.values.aboutText;
-        document.querySelector('.author h2').innerHTML = resultJson.values.author;
-        document.querySelector('.team h2').innerHTML = resultJson.values.team;
-        const names = document.querySelectorAll('.name');
-        names[0].innerHTML = resultJson.values.p1;
-        const works = document.querySelectorAll('.work');
-        works[0].innerHTML = resultJson.values.p1Work;
-        names[1].innerHTML = resultJson.values.p2;
-        works[1].innerHTML = resultJson.values.p2Work;
-        names[2].innerHTML = resultJson.values.p3;
-        works[2].innerHTML = resultJson.values.p3Work;
-        names[3].innerHTML = resultJson.values.p4;
-        works[3].innerHTML = resultJson.values.p4Work;
-        names[4].innerHTML = resultJson.values.p5;
-        works[4].innerHTML = resultJson.values.p5Work;
+        if (document.querySelector('h1')) {
+          document.querySelector('h1').innerHTML = resultJson.values.h1;
+          document.querySelector('.lead').innerHTML = resultJson.values.lead;
+          document.querySelector('.about h2').innerHTML = resultJson.values.about;
+          document.querySelector('.about p').innerHTML = resultJson.values.aboutText;
+          document.querySelector('.author h2').innerHTML = resultJson.values.author;
+          document.querySelector('.team h2').innerHTML = resultJson.values.team;
+          const names = document.querySelectorAll('.name');
+          names[0].innerHTML = resultJson.values.p1;
+          const works = document.querySelectorAll('.work');
+          works[0].innerHTML = resultJson.values.p1Work;
+          names[1].innerHTML = resultJson.values.p2;
+          works[1].innerHTML = resultJson.values.p2Work;
+          names[2].innerHTML = resultJson.values.p3;
+          works[2].innerHTML = resultJson.values.p3Work;
+          names[3].innerHTML = resultJson.values.p4;
+          works[3].innerHTML = resultJson.values.p4Work;
+          names[4].innerHTML = resultJson.values.p5;
+          works[4].innerHTML = resultJson.values.p5Work;
+        }
       });
   };
 
