75b10e81633076a02b68c42e55fbf7adc7a1b291
mbulldozer <mbulldozer@mail.ru>
2019-01-28 16:19:11 +0300

    #6/feat: add templates for author-info page

---
+++ js/gallery.template.js
@@ -0,0 +1,54 @@
+const templateGallery =`
+<section class="gallery">
+        <h2>Галерея</h2>
+        <div class="row">
+          <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+                 
+              </a>
+          </div>
+          <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href=""  class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid"  alt="">
+              </a>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+              </a>
+          </div>
+          
+          <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+              </a>
+          </div>
+          
+           <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+              </a>
+          </div>
+          
+           <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+              </a>
+          </div>
+          
+           <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+              </a>
+          </div>
+          
+           <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="" class="fancybox" rel="ligthbox">
+                  <img  src="" class="zoom img-fluid "  alt="">
+              </a>
+          </div>  
+        </div>
+</section>
+`;
